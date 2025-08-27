85c75b0be45bcd1d166f3b4595601375fbe59313
Frugan <dev@frugan.it>
2025-08-27 08:37:04 +0200

    feat!: synch w/ parent image

--- 8.3/entrypoint-after.sh
+++ 8.3/entrypoint-after.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-set -e
-#set -o errexit
-#set -o nounset
-#set -o pipefail
-#set -o xtrace # Uncomment this line for debugging purpose
+# Bash Strict Mode:
+# -e  / -o errexit    :: Exit immediately if a command exits with a non-zero status.
+# -E  / -o errtrace   :: Inherit ERR trap in functions, subshells, and substitutions (older shells may require -o errtrace instead of -E).
+# -u  / -o nounset    :: Treat unset variables as an error and exit immediately.
+# -o pipefail         :: Exit on error in pipeline.
+# -x  / -o xtrace     :: Print each command and its arguments as they are executed (useful for debugging).
+# -T  / -o functrace  :: Allow function tracing (used for DEBUG and RETURN traps within functions and sourced files).
+#
+# Optional:
+# shopt -s inherit_errexit  :: Bash >= 4.4: ensures ERR trap inheritance in all cases
+#
+# Common practice:
+# set -eEuo pipefail  # Strict mode (recommended)
+set -eEuo pipefail
 
 GCLOUD_CMD=$(which gcloud)
 
